b636d55944a0379fbc42d230c5fc329be0400417
PatrickAngrezani <pangrezani@gmail.com>
2024-10-07 12:08:12 -0300

    feat: implement login

--- internorbit1/urls.py
+++ internorbit1/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from appinternorbit1 import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('posts/', views.post_list),
+    path('posts/', views.post_list, name='post_list'),
     path('posts/create/', views.post_create, name='post_create'),
-    path('posts/<int:pk>/', views.post_detail, name='post-detail'),
-    path('posts/<int:pk>', views.post_detail, name='post-update'),
-    path('posts/<int:pk>/delete/', views.post_delete, name='post-delete'),
+    path('posts/<int:pk>/', views.post_detail, name='post_detail'),
+    path('posts/<int:pk>/update/', views.post_update, name='post_update'),
+    path('posts/delete/<int:pk>/', views.post_delete, name='post_delete'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
