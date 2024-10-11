1a89dd0917a1741e5bed3bd955aed616bd1237e5
PatrickAngrezani <pangrezani@gmail.com>
2024-10-11 01:03:39 -0300

    feat: configure routes to receive frontend

--- internorbit1/settings.py
+++ internorbit1/settings.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,14 +10,17 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'appinternorbit1',
-    'ckeditor',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "corsheaders",
+    "ckeditor",
+    "rest_framework",
+    "rest_framework_simplejwt.token_blacklist",
+    "appinternorbit1",
 ]
 
 CKEDITOR_CONFIGS = {
@@ -25,6 +29,20 @@ CKEDITOR_CONFIGS = {
         'height': 300,
         'width': '100%',
     },
+}
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
 }
 
 MIDDLEWARE = [
--- internorbit1/urls.py
+++ internorbit1/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from appinternorbit1 import views
+from django.contrib.auth import views as auth_views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView)
 
 urlpatterns = [
-    path('posts/', views.post_list),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('posts/', views.post_list, name='post_list'),
     path('posts/create/', views.post_create, name='post_create'),
     path('posts/<int:pk>/', views.post_detail, name='post-detail'),
     path('posts/<int:pk>', views.post_detail, name='post-update'),
