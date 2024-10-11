f42b592e7ce404bd4d1493d74a736f547b5ac8b6
PatrickAngrezani <pangrezani@gmail.com>
2024-10-11 01:51:29 -0300

    feat: update model routes

--- appinternorbit1/frontend/src/services/postService.js
+++ appinternorbit1/frontend/src/services/postService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_API_POSTS_URL = "http://127.0.0.1:8000/posts";
+const BASE_API_POSTS_URL = "http://127.0.0.1:8000/posts/";
 
 const getAuthHeader = () => {
   const token = localStorage.getItem("authToken");
@@ -8,24 +8,23 @@ const getAuthHeader = () => {
 };
 
 const getPosts = () => {
-  return axios.get(`${BASE_API_POSTS_URL}/`);
+  return axios.get(`${BASE_API_POSTS_URL}`);
 };
 
 const createPost = (postData) => {
-  const createPostURL = `${BASE_API_POSTS_URL}/create/`;
+  const createPostURL = `${BASE_API_POSTS_URL}create/`;
 
   return axios.post(createPostURL, postData, { headers: getAuthHeader() });
 };
 
 const updatePost = (id, updatedData) => {
-  console.log({updateid: id});
+  const updatePostURL = `${BASE_API_POSTS_URL}${id}/update/`;
   
-  const updatePostURL = `${BASE_API_POSTS_URL}/${id}/update/`;
   return axios.put(updatePostURL, updatedData, { headers: getAuthHeader() });
 };
 
 const getPostById = (id) => {
-  return axios.get(`${BASE_API_POSTS_URL}/${id}`);
+  return axios.get(`${BASE_API_POSTS_URL}${id}`);
 };
 
 export { getPosts, createPost, getAuthHeader, updatePost, getPostById };
