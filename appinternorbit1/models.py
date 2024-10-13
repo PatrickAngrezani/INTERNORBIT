c33feb8eac32d8310b57e592a4fdfe9d0a5f88ce
PatrickAngrezani <pangrezani@gmail.com>
2024-10-13 13:00:16 -0300

    feat: comment model

--- appinternorbit1/models.py
+++ appinternorbit1/models.py
@@ -9,3 +9,9 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
